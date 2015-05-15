56b841edb2f70ecfcfb9bf62b8be8600e8a078c0
Zornica <zornicaewgeniewa@gmail.com>
2015-05-15 17:33:44 +0300

    ready

--- src/task5/Demo.java
+++ src/task5/Demo.java
@@ -8,15 +8,15 @@ import java.util.Hashtable;
  * Created by Zornitsa Petkova on 5/12/15.
  */
 public class Demo {
-  public static void main(String[] args)throws InterruptedException{
+  public static void main(String[] args) throws InterruptedException {
     TimeHashTable table = new TimeHashTable(3);
 
-    table.put("1","Sisa");
+    table.put("1", "Sisa");
     System.out.println("Slagame 1");
     Thread.sleep(1000);
     System.out.println(" ");
 
-    table.put("2","Ivan");
+    table.put("2", "Ivan");
     System.out.println("Slagame 2");
     Thread.sleep(1000);
     System.out.println(" ");
@@ -25,22 +25,22 @@ public class Demo {
     table.get("1");
     System.out.println(" ");
 
-    table.put("3","Koko");
+    table.put("3", "Koko");
     System.out.println("Slagame 3");
     Thread.sleep(1000);
     System.out.println(" ");
 
-    table.put("4","Stoqn");
+    table.put("4", "Stoqn");
     System.out.println("Slagame 4");
     Thread.sleep(1000);
     System.out.println(" ");
 
     System.out.println("Prezarejdame 1");
-    table.put("1","Mima");
+    table.put("1", "Mima");
     System.out.println(" ");
 
 
-    table.put("5","Petyr");
+    table.put("5", "Petyr");
     System.out.println("Slagame 5");
     Thread.sleep(1000);
     System.out.println(" ");
